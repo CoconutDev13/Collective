f23f0938a94c6cd97e20193b5f88f8bec2ee80d7
Kyriakos <rastamansid12@gmail.com>
2023-04-12 23:53:53 +0300

    implemented help command

--- Commands/Info/help.js
+++ Commands/Info/help.js
@@ -18,9 +18,92 @@ module.exports = {
     };
 
     const directories = [
-        ...new Set(interaction.client.commands.mpa((cmd) => cmd.folder)),
+      ...new Set(interaction.client.commands.mpa((cmd) => cmd.folder)),
     ];
 
-    
+    const formatString = (str) =>
+      `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+
+    const categories = directories.map((dir) => {
+      const getCommands = interaction.client.commands
+        .filter((cmd) => cmd.folder === dir)
+        .map((cmd) => {
+          return {
+            name: cmd.data.name,
+            description:
+              cmd.data.description ||
+              "There is not description for this command.",
+          };
+        });
+
+      return {
+        directory: formatString(dir),
+        commands: getCommands,
+      };
+    });
+
+    const embed = new EmbedBuilder().setDescription(
+      "Please choose a category in the dropdown menu"
+    );
+
+    const components = (state) => [
+      new ActionRowBuilder().addComponents(
+        new SelectMenuBuilder()
+          .setCustomId("help-menu")
+          .setPlaceholder("Please select a category")
+          .setDisabled(state)
+          .addOptions(
+            categories.map((cmd) => {
+              return {
+                label: cmd.directory,
+                value: cmd.directory.toLocaleLowerCase(),
+                description: `Commands from ${cmd.directory} category`,
+                emoji: emojis[cmd.directory.toLocaleLowerCase() || null],
+              };
+            })
+          )
+      ),
+    ];
+
+    const initialMessage = await interaction.reply({
+      embeds: [embed],
+      components: components(false),
+    });
+
+    const filter = (interaction) =>
+      interaction.user.id === interaction.member.id;
+
+    const collector = interaction.channel.createMessageComponentCollector({
+      filter,
+      ComponentType: ComponentType.SelectMenu,
+    });
+
+    collector.on("collect", (interaction) => {
+      const [directory] = interaction.values;
+      const category = categories.find(
+        (x) => x.directory.toLocaleLowerCase() === directory
+      );
+
+      const categoryEmbed = new EmbedBuilder()
+        .setTitle(`${formatString(directory)} commands`)
+        .setDescription(
+          `A list of all the commands categorized under ${directory}`
+        )
+        .addFields(
+          category.commands.map((cmd) => {
+            return {
+              name: `\`${cmd.name}\``,
+              value: cmd.description,
+              inline: true,
+            };
+          })
+        );
+
+        interaction.update({embeds: [categoryEmbed]});
+    });
+
+    collector.on("end", () => {
+        initialMessage.edit({components: components(ture)});
+    });
   },
 };
