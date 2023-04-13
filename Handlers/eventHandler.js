c56ea91e83562db72d1200b41166c3f37b979ab1
CoconutDev13 <bakedbacon4@gmail.com>
2023-04-13 05:52:27 +0200

    slashCommandInteraction

--- Handlers/eventHandler.js
+++ Handlers/eventHandler.js
@@ -1,9 +1,11 @@
+const fs = require('fs')
+const ascii = require('ascii-table');
+
 function loadEvents(client) {
-    const ascii = require('ascii-table');
-    const fs = require('fs')
     const table = new ascii().setHeading('Events', 'Status');
 
     const folders = fs.readdirSync('./Events');
+
     for (const folder of folders) {
         const files = fs.readdirSync(`./Events/${folder}`).filter((file) => file.endsWith(".js"));
 
@@ -12,23 +14,21 @@ function loadEvents(client) {
 
             if (event.rest) {
                 if(event.once)
-                    client.rest.once(event.name, (...args) =>
-                    event.execute(...args, client)
-                );
+                    client.rest.once(event.name, (...args) => event.execute(...args, client));
                 else
-                client.rest.on(event.name, (...args) =>
-                    event.execute(...args, client)
-                );
+                    client.rest.on(event.name, (...args) => event.execute(...args, client));
             } else {
                 if (event.once)
                     client.once(event.name, (...args) => event.execute(...args, client));
-                else client.on(event.name, (...args) => event.execute(...args, client));
+                else
+                    client.on(event.name, (...args) => event.execute(...args, client));
             }
+            
             table.addRow(file, "loaded")
-            continue;
         }
     }
-    return console.log(table.toString(), "\nLoaded Events");
+    
+    console.log(table.toString(), "\nLoaded Events");
 }
 
 module.exports = {loadEvents}
